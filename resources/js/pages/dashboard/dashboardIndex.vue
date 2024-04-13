3f2c7b015295ef7359d1519517a3e4690c85a5ed
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-04-13 16:06:04 +0000

    add modification

--- resources/js/pages/dashboard/dashboardIndex.vue
+++ resources/js/pages/dashboard/dashboardIndex.vue
@@ -133,6 +133,65 @@
             <li class="relative px-6 py-3">
                 <button
                   class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                  @click="toggleCatMenu"
+                  aria-haspopup="true"
+                >
+                  <span class="inline-flex items-center">
+                    <svg
+                      class="w-5 h-5"
+                      aria-hidden="true"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"
+                      ></path>
+                    </svg>
+                    <span class="ml-4">Catégorie Crud</span>
+                  </span>
+                  <svg
+                    class="w-4 h-4"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+                <template v-if="isCatMenuOpen">
+                  <ul
+                    class="p-2 mt-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-red dark:text-gray-400 dark:bg-gray-900"
+                    aria-label="submenu"
+                  >
+                    <li
+                      class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    >
+                      <router-link class="w-full" to="/dashboard/animal_add">Add Catégorie</router-link>
+                    </li>
+                    <li
+                      class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    >
+                      <router-link class="w-full" to="/dashboard/animal_update">Update Catégorie</router-link>
+                    </li>
+                    <li
+                      class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    >
+                      <router-link class="w-full" to="/dashboard/animal_delete">Delete Catégorie</router-link>
+                    </li>
+                  </ul>
+                </template>
+            </li>
+            <li class="relative px-6 py-3">
+                <button
+                  class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                   @click="toggleAuthMenu"
                   aria-haspopup="true"
                 >
@@ -516,7 +575,8 @@ export default {
     data() {
         return {
             isFormsMenuOpen: false,
-            isAuthMenuOpen: false
+            isAuthMenuOpen: false,
+            isCatMenuOpen: false
         };
     },
     methods: {
@@ -525,6 +585,9 @@ export default {
         },
         toggleAuthMenu() {
             this.isAuthMenuOpen = !this.isAuthMenuOpen;
+        },
+        toggleCatMenu() {
+            this.isCatMenuOpen = !this.isCatMenuOpen;
         }
     }
 }
