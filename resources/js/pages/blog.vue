3a1647f775c3923cdf6f4782c6e9060c8cb7477a
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-04-17 17:32:43 +0100

    add modif

--- resources/js/pages/blog.vue
+++ resources/js/pages/blog.vue
@@ -35,12 +35,12 @@
                 <div class="absolute bottom-0 left-0 p-6">
                   <h2 class="text-xl font-semibold 5 text-white">The Decorated Ways</h2>
                   <p class="text-base leading-4 text-white mt-2">Dive into minimalism</p>
-                  <a href="javascript:void(0)" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
+                  <router-link :to="`/blog/${blogs[1].id}/animal`" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                     <p class="pr-2 text-sm font-medium leading-none">Read More</p>
                     <svg class="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
-                  </a>
+                  </router-link>
                 </div>
               </div>
               <img :src="'storage/images/' + blogs[1]?.image.url" class="w-full" alt="wall design" />
@@ -52,12 +52,12 @@
               <div class="absolute bottom-0 left-0 md:p-10 p-6">
                 <h2 class="text-xl font-semibold 5 text-white">The Decorated Ways</h2>
                 <p class="text-base leading-4 text-white mt-2">Dive into minimalism</p>
-                <a href="javascript:void(0)" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
+                <router-link :to="`/blog/${blogs[2].id}/animal`" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                   <p class="pr-2 text-sm font-medium leading-none">Read More</p>
                   <svg class="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                   </svg>
-                </a>
+                </router-link>
               </div>
             </div>
             <img :src="'storage/images/' + blogs[2]?.image.url" alt="sitting place" class="w-full mt-8 md:mt-6 hidden sm:block" />
@@ -71,12 +71,12 @@
               <div class="absolute bottom-0 left-0 md:p-10 p-6">
                 <h2 class="text-xl font-semibold 5 text-white">The Decorated Ways</h2>
                 <p class="text-base leading-4 text-white mt-2">Dive into minimalism</p>
-                <a href="javascript:void(0)" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
+                <router-link :to="`/blog/${blogs[3].id}/animal`" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                   <p class="pr-2 text-sm font-medium leading-none">Read More</p>
                   <svg class="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                   </svg>
-                </a>
+                </router-link>
               </div>
             </div>
             <img :src="'storage/images/' + blogs[3]?.image.url" alt="sitting place" class="w-full sm:block hidden" />
@@ -89,12 +89,12 @@
                 <div class="absolute bottom-0 left-0 p-6">
                   <h2 class="text-xl font-semibold 5 text-white">The Decorated Ways</h2>
                   <p class="text-base leading-4 text-white mt-2">Dive into minimalism</p>
-                  <a href="javascript:void(0)" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
+                  <router-link :to="`/blog/${blogs[4].id}/animal`" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                     <p class="pr-2 text-sm font-medium leading-none">Read More</p>
                     <svg class="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
-                  </a>
+                  </router-link>
                 </div>
               </div>
               <img :src="'storage/images/' + blogs[4]?.image.url" class="w-full" alt="chair" />
@@ -105,12 +105,12 @@
                 <div class="absolute bottom-0 left-0 p-6">
                   <h2 class="text-xl font-semibold 5 text-white">The Decorated Ways</h2>
                   <p class="text-base leading-4 text-white mt-2">Dive into minimalism</p>
-                  <a href="javascript:void(0)" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
+                  <router-link :to="`/blog/${blogs[5].id}/animal`" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                     <p class="pr-2 text-sm font-medium leading-none">Read More</p>
                     <svg class="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
-                  </a>
+                  </router-link>
                 </div>
               </div>
               <img  :src="'storage/images/'+blogs[0]?.image.url " class="w-full" alt="wall design" />
